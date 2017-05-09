0c189a70d3fd81f3073c681f621e41ef10e98e61
Wolfgang Hoschek <whoschek@cloudera.com>
2017-05-09 08:47:13 -0700

    CDH-42213: Nomore ship solrCell morphline command by default, flume part2. Also update to solr6 and fix unit tests
    Change-Id: Ib09a87090ef654344c902364bb6eaccf2ca78c23

--- flume-ng-sinks/flume-ng-morphline-solr-sink/src/test/java/org/apache/flume/sink/solr/morphline/TestMorphlineSolrSink.java
+++ flume-ng-sinks/flume-ng-morphline-solr-sink/src/test/java/org/apache/flume/sink/solr/morphline/TestMorphlineSolrSink.java
@@ -91,9 +91,10 @@ public class TestMorphlineSolrSink extends SolrTestCaseJ4 {
   @BeforeClass
   public static void beforeClass() throws Exception {
     initCore(
-        RESOURCES_DIR + "/solr/collection1/conf/solrconfig.xml", 
-        RESOURCES_DIR + "/solr/collection1/conf/schema.xml",
-        RESOURCES_DIR + "/solr");
+        "solrconfig.xml",
+        "schema.xml",
+        RESOURCES_DIR + "/solr"
+        );
   }
 
   @Before
@@ -121,7 +122,7 @@ public class TestMorphlineSolrSink extends SolrTestCaseJ4 {
       //solrServer = new HttpSolrServer(EXTERNAL_SOLR_SERVER_URL);
     } else {
       if (TEST_WITH_EMBEDDED_SOLR_SERVER) {
-        solrServer = new TestEmbeddedSolrServer(h.getCoreContainer(), "");
+        solrServer = new TestEmbeddedSolrServer(h.getCoreContainer(), "collection1");
       } else {
         throw new RuntimeException("Not yet implemented");
         //solrServer = new TestSolrServer(getSolrServer());
